e6c07db5c23eaa6373054fb51c794b81f1b2c1b3
Ryan Moore <talkingstove@gmail.com>
2017-07-16 00:47:42 -0700

    cleanup

--- framework/scripts/app.build.js
+++ framework/scripts/app.build.js
@@ -1,16 +1,16 @@
 ({
     baseUrl: '../src',
   	out: '../out/L.js',
-    include: ["lagomorph"],
-  	shim: {
-        'jQuery': {
-            exports: '$'
-        },
-        'lagomorph': {
-            exports: 'L'
-        },
-        'test2': {
-            exports: 'test2'
-        }
-    }
+    include: ["lagomorph"] //the main app file, which requires the rest
+  	// shim: {
+   //      'jQuery': {
+   //          exports: '$'
+   //      },
+   //      'lagomorph': {
+   //          exports: 'L'
+   //      },
+   //      'test2': {
+   //          exports: 'test2'
+   //      }
+   //  }
 })
